af12c327770b530729318edddded5eb20b6e848f
Ian Goodrich and Sara Hamilton <ian@>
2018-01-24 09:56:12 -0800

    add Square and Board functions

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+function Square(row, column){
+  this.row = row;
+  this.column = column;
+  this.marker = "";
+}
+
+function Board() {
+  this.square1 = new Square("1", "A");
+  this.square2 = new Square("2", "A");
+  this.square3 = new Square("3", "A");
+  this.square4 = new Square("1", "B");
+  this.square5 = new Square("2", "B");
+  this.square6 = new Square("3", "B");
+  this.square7 = new Square("1", "C");
+  this.square8 = new Square("2", "C");
+  this.square9 = new Square("3", "C");
+}
+
+
+gameBoard = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var test1 = new Square("1", "A")
+var test2 = new Square("1", "B")
+var test3 = new Square("1", "C")
+var test4 = new Board();
+
+gameBoard.push(test4)
+$('document').ready(function(){
+
+
+
+
+
+
+
+});
