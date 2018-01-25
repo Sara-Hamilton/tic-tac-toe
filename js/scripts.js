38f1693af6d8f6dd3611ed5c40a76539d0b041d7
Ian Goodrich and Sara Hamilton <ian@>
2018-01-24 16:54:28 -0800

    begin front end and add jquery logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
-function Square(marker) {
+function Square(name) {
+  this.name = name;
   this.marker = "";
 }
 
 function Board() {
-  this.row1 = [square1 = new Square(), square2 = new Square(), square3 = new Square("O")];
-  this.row2 = [square4 = new Square(), square5 = new Square("O"), square6 = new Square()];
-  this.row3 = [square7 = new Square("O"), square8 = new Square(), square9 = new Square()];
-  this.column1 = [square1, square4, square7];
-  this.column2 = [square2, square5, square8];
-  this.column3 = [square3, square6, square9];
-  this.diagonal1 = [square1, square5, square9];
-  this.diagonal2 = [square3, square5, square7];
+  this.square1 = new Square("square1");
+  this.square2 = new Square("square2");
+  this.square3 = new Square("square3");
+  this.square4 = new Square("square4");
+  this.square5 = new Square("square5");
+  this.square6 = new Square("square6");
+  this.square7 = new Square("square7");
+  this.square8 = new Square("square8");
+  this.square9 = new Square("square9");
+  this.row1 = [this.square1, this.square2, this.square3];
+  this.row2 = [this.square4, this.square5, this.square6];
+  this.row3 = [this.square7, this.square8, this.square9];
+  this.column1 = [this.square1, this.square4, this.square7];
+  this.column2 = [this.square2, this.square5, this.square8];
+  this.column3 = [this.square3, this.square6, this.square9];
+  this.diagonal1 = [this.square1, this.square5, this.square9];
+  this.diagonal2 = [this.square3, this.square5, this.square7];
   this.trios = [this.row1, this.row2, this.row3, this.column1, this.column2, this.column3, this.diagonal1, this.diagonal2];
 }
 
@@ -31,12 +41,9 @@ Board.prototype.winCheck = function() {
 Board.prototype.gameOverCheck = function() {
   for (var i = 0; i < this.trios.length; i++) {
     for (var j = 0; j < 3; j++) {
-      console.log(i, j)
       if (this.trios[i][j].marker === "") {
-        console.log(this.trios[i][j], 'false')
         return false;
       } else {
-        console.log("true");
         return true;
       }
     }
@@ -75,11 +82,19 @@ game.row3[0].marker = "O";
 game.gameOverCheck();
 
 $('document').ready(function() {
+  $("#playerForm").submit(function(event) {
+    event.preventDefault();
+    var playerXName = $("#playerX").val();
+    var playerOName = $("#playerO").val();
+    $("#playerForm").fadeOut(300, "swing");
+    $("#board").delay(300).fadeIn(300, "swing");
+    var playerX = new Player(playerXName, "X");
+    var playerO = new Player(playerOName, "O");
+    $(".square").click(function() {
+      var clickedSquare  = $(this).attr('id');
+      // write function to iterate through squares and find the one whose name === clickedSquare
+      console.log(clickedSquare === game.square1.name);
+      });
 
-
-
-
-
-
-
+    });
 });
